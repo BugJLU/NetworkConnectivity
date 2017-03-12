75cfb62f4e59e8b66f9c4f2c1653d2c367d1b170
Ownsky <wangguoyu7777@me.com>
2017-03-12 13:11:43 +0800

    add funcs: checkRedundancy, checkReliability, keyNode

--- NetworkConnectivity.py
+++ NetworkConnectivity.py
@@ -7,6 +7,8 @@ class NetworkGraph(object):
 		self.nodes = list(networkGraph.nodes)
 		self.edges = list(networkGraph.edges)
 		self.adjMat = y = [[j for j in i] for i in x]
+	def __init__(self, node_num):
+		adjMat = [[0]*node_num for i in range(node_num)]
 	def __init__(self):
 		self.nodes = []
 		self.edges = []
@@ -30,16 +32,21 @@ class NetworkGraph(object):
 			#TODO: Modify the adjMat
 	def removeNodes(self, nodes):
 		for i in nodes:
+			k = self.nodes.index(i)
 			self.nodes.remove(i)
-			#TODO: Modify the adjMat
+			for j in range(len(self.nodes)):
+				adjMat[j][k] = adjMat[k][j] = 0
+			#TODO: check
 	def addEdges(self, edges):
 		for i in edges:
 			self.edges.append(i)
-			#TODO: Modify the adjMat
+			adjMat[edges[i][0]][edges[i][1]] += 1
+			#TODO: check
 	def removeEdges(self, edges):
 		for i in edges:
 			self.edges.remove(i)
-			#TODO: Modify the adjMat
+			adjMat[edges[i][0]][edges[i][1]] -= 1
+			#TODO: check
 	def draw(self):
 		G = nx.Graph()
 		G.add_nodes_from(self.nodes)
@@ -52,7 +59,37 @@ class NetworkGraph(object):
 	def connectiveBetween(self, nodeA, nodeB):
 		#TODO
 		pass
+	def checkRedundancy(self):
+		rEdges = []
+		tG = NetworkGraph(len(self.nodes))
+		tG.addNodes(self.nodes)
+		for x in self.nodes:
+			if tG.connectiveBetween(x[0], x[1]):
+				rEdges.append(x)
+			else:
+				tG.addEdges([x])
+		return tG, rEdges
 
+	def checkReliability(self):
+		kEdges = []
+		tG = NetworkGraph(self)
+		result = false
+		for x in self.edges:
+			tG.removeEdges([x])
+			if not tG.connective():
+				kEdges.append(x)
+				result = true
+			tG.addEdges([x])
+		return result, kEdges
+
+	def keyNode(self):
+		kNode = []
+		for x in self.nodes:
+			tG = NetworkGraph(self)
+			tG.removeNodes([x])
+			if not tG.connective():
+				kNode.append(x)
+		return kNode
 
 NG = NetworkGraph()
 NG.randomGenerator(int(input()), int(input()))
