69f9cd814c816ba93387c22ba4b1c528c45e1378
HanYec <i@qcblog.cn>
2017-03-15 00:08:26 +0800

    Add a simple Web interface.

---
+++ WebMain.py
@@ -0,0 +1,96 @@
+from NetworkConnectivity import NetworkGraph
+import networkx as nx
+import bottle
+
+graph = NetworkGraph()
+down_buf = {}
+
+
+def tojson(graph):
+    nodes = []
+    edges = []
+    g = nx.Graph()
+    g.add_nodes_from(graph.nodes)
+    g.add_edges_from(graph.edges)
+    pos = nx.spring_layout(g, scale=500.0)
+
+    for node in graph.nodes:
+        nodes.append({'id': node,
+                      'label': str(node),
+                      'x': int(pos[node][0]),
+                      'y': int(pos[node][1]),
+                      'size': 3,
+                      'color': '#B30000'})
+    for i, val in enumerate(graph.edges):
+        edges.append({'id': i,
+                      'source': val[0],
+                      'target': val[1],
+                      'color': '#B30000'})
+    return {'nodes': nodes, 'edges': edges}
+
+
+@bottle.route('/')
+def index():
+    return bottle.static_file('index.html', root='./assets/')
+
+
+@bottle.route('/assets/:path#.+#')
+def assets(path):
+    return bottle.static_file(path, root='./assets/')
+
+
+@bottle.route('/generate')
+def generate():
+    try:
+        node_num = int(bottle.request.params.get("node_num", None))
+        edge_num = int(bottle.request.params.get("edge_num", None))
+    except:
+        return {'result': False}
+    graph.random_generator(node_num, edge_num)
+    while not graph.connective():
+        graph.random_generator(node_num, edge_num)
+    return {'result': True}
+
+
+@bottle.route('/redundancy')
+def redundancy():
+    global down_buf
+    result = []
+    g, edges = graph.check_redundancy()
+    for e in edges:
+        result.append(graph.edges.index(e))
+    down_buf = tojson(g)
+    return {'result': result}
+
+
+@bottle.route('/reliability')
+def reliability():
+    global down_buf
+    result = []
+    g, edges = graph.check_reliability()
+    for e in edges:
+        result.append(graph.edges.index(e))
+    down_buf = tojson(g)
+    return {'result': result}
+
+
+@bottle.route('/key_node')
+def key_node():
+    result = []
+    nodes = graph.key_node()
+    return {'result': nodes}
+
+
+@bottle.route('/json')
+def json():
+    global down_buf
+    down_buf = tojson(graph)
+    return down_buf
+
+
+@bottle.route('/download')
+def download():
+    return down_buf
+
+
+bottle.run(host='localhost', port=8080)
---
+++ assets/main.js
@@ -0,0 +1,170 @@
+var s = null;
+
+$(document).ready(function () {
+
+    function isArrayFn(value) {
+        if (typeof Array.isArray === "function") {
+            return Array.isArray(value);
+        } else {
+            return Object.prototype.toString.call(value) === "[object Array]";
+        }
+    }
+
+    sigma.classes.graph.addMethod('neighbors', function (nodeId) {
+        var k,
+            neighbors = {},
+            index = this.allNeighborsIndex[nodeId] || {};
+
+        for (k in index)
+            neighbors[k] = this.nodesIndex[k];
+
+        return neighbors;
+    });
+
+    s = new sigma('container');
+
+    function refresh() {
+        sigma.parsers.json(
+            '/json', s,
+            function () {
+                s.graph.nodes().forEach(function (n) {
+                    n.originalColor = n.color;
+                });
+                s.graph.edges().forEach(function (e) {
+                    e.originalColor = e.color;
+                });
+
+                s.refresh();
+            }
+        );
+    }
+
+    $('#generate').click(function () {
+        $.ajax({
+            url: '/generate',
+            data: {
+                node_num: $('#node_num').val(),
+                edge_num: $('#edge_num').val()
+            },
+            success: function (data) {
+                if (data['result'] == true) {
+                    refresh();
+                } else {
+                    alert("Numbers Error!")
+                }
+            }
+        });
+    });
+
+    $('#refresh').click(function () {
+        refresh();
+    });
+
+    $('#redundancy').click(function () {
+        $.ajax({
+            url: '/redundancy',
+            success: function (data) {
+                if (isArrayFn(data['result'])) {
+                    data['result'].forEach(function (e) {
+                        s.graph.edges()[e].color = '#c79c9c';
+                    });
+                    s.refresh();
+                } else {
+                    alert("Check Redundancy Error!")
+                }
+            }
+        });
+    });
+
+    $('#reliability').click(function () {
+        $.ajax({
+            url: '/reliability',
+            success: function (data) {
+                if (isArrayFn(data['result'])) {
+                    data['result'].forEach(function (e) {
+                        s.graph.edges()[e].color = '#c79c9c';
+                    });
+                    s.refresh();
+                } else {
+                    alert("Check Reliability Error!")
+                }
+            }
+        });
+    });
+
+    $('#key_node').click(function () {
+        $.ajax({
+            url: '/key_node',
+            success: function (data) {
+                if (isArrayFn(data['result'])) {
+                    data['result'].forEach(function (e) {
+                        s.graph.nodes()[e].color = '#c79c9c';
+                    });
+                    s.refresh();
+                } else {
+                    alert("Check Key Node Error!")
+                }
+            }
+        });
+    });
+
+    s.bind('clickStage', function (e) {
+        s.graph.nodes().forEach(function (n) {
+            n.color = n.originalColor;
+        });
+
+        s.graph.edges().forEach(function (e) {
+            e.color = e.originalColor;
+        });
+
+        // Same as in the previous event:
+        s.refresh();
+    });
+
+    s.bind('clickNode', function (e) {
+        var nodeId = e.data.node.id,
+            toKeep = s.graph.neighbors(nodeId);
+        toKeep[nodeId] = e.data.node;
+
+        s.graph.nodes().forEach(function (n) {
+            if (toKeep[n.id])
+                n.color = n.originalColor;
+            else
+                n.color = '#c79c9c';
+        });
+
+        s.graph.edges().forEach(function (e) {
+            if (toKeep[e.source] && toKeep[e.target])
+                e.color = e.originalColor;
+            else
+                e.color = '#c79c9c';
+        });
+
+        s.refresh();
+
+    });
+});
+
+
+/*sigma.parsers.json(
+ '/json',
+ {
+ container: 'container'
+ },
+ function (s) {
+ sigma_instance = s;
+
+ s.graph.nodes().forEach(function (n) {
+ n.originalColor = n.color;
+ });
+ s.graph.edges().forEach(function (e) {
+ e.originalColor = e.color;
+ });
+
+
+ }
+ );*/
+
+
+
+
