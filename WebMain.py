ca614831e25f0a70a7ac7f2b63e84b35d7a00692
HYec <hyc320@qq.com>
2017-04-05 00:16:14 +0800

    Change Web UI. Add opeation for nonredundant graph.

--- WebMain.py
+++ WebMain.py
@@ -3,29 +3,35 @@ import networkx as nx
 import bottle
 
 graph = NetworkGraph()
-down_buf = {}
+last_graph = None
+layout = {}
 
 
-def tojson(graph):
-    nodes = []
-    edges = []
+def do_layout(scale=500.0):
+    global graph, layout
     g = nx.Graph()
     g.add_nodes_from(graph.nodes)
     g.add_edges_from(graph.edges)
-    pos = nx.spring_layout(g, scale=500.0)
+    layout = nx.spring_layout(g, scale=scale)
 
-    for node in graph.nodes:
-        nodes.append({'id': node,
-                      'label': str(node),
-                      'x': int(pos[node][0]),
-                      'y': int(pos[node][1]),
-                      'size': 3,
-                      'color': '#B30000'})
-    for i, val in enumerate(graph.edges):
-        edges.append({'id': i,
-                      'source': val[0],
-                      'target': val[1],
-                      'color': '#B30000'})
+
+def to_json(network_graph):
+    nodes = []
+    edges = []
+    for node in network_graph.nodes:
+        nodes.append({
+            'id': node,
+            'label': str(node),
+            'x': int(layout[node][0]),
+            'y': int(layout[node][1]),
+            'size': 3
+        })
+    for i, val in enumerate(network_graph.edges):
+        edges.append({
+            'id': i,
+            'source': val[0],
+            'target': val[1]
+        })
     return {'nodes': nodes, 'edges': edges}
 
 
@@ -41,57 +47,58 @@ def assets(path):
 
 @bottle.route('/generate')
 def generate():
+    global graph, last_graph
     try:
         node_num = int(bottle.request.params.get("node_num", None))
         edge_num = int(bottle.request.params.get("edge_num", None))
     except:
-        return {'result': False}
-    graph.random_generator(node_num, edge_num)
-    while not graph.connective():
+        return {'result': False, 'error': 'Parameter error.'}
+    try_times = 0
+    while True:
         graph.random_generator(node_num, edge_num)
+        try_times += 1
+        if graph.connective():
+            break
+        if try_times >= 5:
+            return {'result': False, 'error': 'Unconnective.'}
+    do_layout()
+    last_graph = graph
     return {'result': True}
+
+
+@bottle.route('/raw_graph')
+def raw_graph():
+    global graph, last_graph
+    last_graph = graph
+    return {'graph': to_json(graph)}
 
 
 @bottle.route('/redundancy')
 def redundancy():
-    global down_buf
+    global graph, last_graph
     result = []
     g, edges = graph.check_redundancy()
     for e in edges:
         result.append(graph.edges.index(e))
-    down_buf = tojson(g)
-    return {'result': result}
+    last_graph = g
+    return {'graph': to_json(g)}
 
 
 @bottle.route('/reliability')
 def reliability():
-    global down_buf
-    result = []
-    _, edges = graph.check_reliability()
+    global last_graph
+    result, edges = last_graph.check_reliability()
+    json = {'result': result, 'graph': to_json(last_graph), 'edges': []}
     for e in edges:
-        result.append(graph.edges.index(e))
-    down_buf = {'reliability_edge': edges}
-    return {'result': result}
+        json['edges'].append(last_graph.edges.index(e))
+    return json
 
 
 @bottle.route('/key_node')
 def key_node():
-    global down_buf
-    nodes = graph.key_node()
-    down_buf = {'key_node': nodes}
+    global last_graph
+    nodes = last_graph.key_node()
     return {'result': nodes}
-
-
-@bottle.route('/json')
-def json():
-    global down_buf
-    down_buf = tojson(graph)
-    return down_buf
-
-
-@bottle.route('/download')
-def download():
-    return down_buf
 
 
 bottle.run(host='localhost', port=8080)
--- assets/main.js
+++ assets/main.js
@@ -23,20 +23,32 @@ $(document).ready(function () {
 
     s = new sigma('container');
 
-    function refresh() {
-        sigma.parsers.json(
-            '/json', s,
-            function () {
-                s.graph.nodes().forEach(function (n) {
-                    n.originalColor = n.color;
-                });
-                s.graph.edges().forEach(function (e) {
-                    e.originalColor = e.color;
-                });
+    function read(graph, callback) {
+        s.graph.clear();
+        s.graph.read(graph);
 
-                s.refresh();
+        function set_color(o) {
+            if (o.color === undefined) {
+                o.color = '#B30000';
             }
-        );
+            o.originalColor = o.color;
+        }
+
+        if (callback)
+            callback(s);
+
+        s.graph.nodes().forEach(set_color);
+        s.graph.edges().forEach(set_color);
+
+        s.refresh();
+    }
+
+    function try_save(content, filename) {
+
+        if ($('#ask_download')[0].checked &&
+            confirm('Do you want to save file as "' + filename + '"?')) {
+            window.open('data:application/json:' + JSON.stringify(content), '_blank');
+        }
     }
 
     $('#generate').click(function () {
@@ -47,47 +59,70 @@ $(document).ready(function () {
                 edge_num: $('#edge_num').val()
             },
             success: function (data) {
-                if (data['result'] == true) {
-                    refresh();
+                if (data['result'] === true) {
+                    $.ajax({
+                        url: '/raw_graph',
+                        success: function (data) {
+                            read(data['graph']);
+                            try_save(data, 'random.json');
+                        }
+                    });
+
                 } else {
-                    alert("Numbers Error!")
+                    alert("Generate Fail!");
                 }
             }
         });
     });
 
     $('#refresh').click(function () {
-        refresh();
+        $.ajax({
+            url: '/raw_graph',
+            success: function (data) {
+                read(data['graph']);
+                try_save(data, 'raw_graph.json');
+            }
+        });
+        //refresh();
     });
 
     $('#redundancy').click(function () {
         $.ajax({
             url: '/redundancy',
             success: function (data) {
-                if (isArrayFn(data['result'])) {
-                    data['result'].forEach(function (e) {
-                        s.graph.edges()[e].color = '#c79c9c';
-                    });
-                    s.refresh();
-                } else {
-                    alert("Check Redundancy Error!")
-                }
+                read(data['graph']);
+                try_save(data, 'no_redundancy.json');
             }
         });
+        /*$.ajax({
+         url: '/redundancy',
+         success: function (data) {
+         if (isArrayFn(data['result'])) {
+         data['result'].forEach(function (e) {
+         s.graph.edges()[e].color = '#c79c9c';
+         });
+         s.refresh();
+         } else {
+         alert("Check Redundancy Error!")
+         }
+         }
+         });*/
     });
 
     $('#reliability').click(function () {
         $.ajax({
             url: '/reliability',
             success: function (data) {
-                if (isArrayFn(data['result'])) {
-                    data['result'].forEach(function (e) {
-                        s.graph.edges()[e].color = '#000';
-                    });
-                    s.refresh();
+                if (data['result'] === true) {
+                    alert('The graph is reliable.');
                 } else {
-                    alert("Check Reliability Error!")
+                    read(data['graph'], function (s) {
+                        data['edges'].forEach(function (e) {
+                            s.graph.edges()[e].color = '#000';
+                        });
+                    });
                 }
+                try_save(data, 'reliability.json')
             }
         });
     });
